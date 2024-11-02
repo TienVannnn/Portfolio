f303ec7383eaff8da5a563dd058dcd3a0f1e1b13
Tiến <letien3413@gmail.com>
2024-11-02 19:00:21 +0700

    add HeroSection

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="bg-[#111827] min-h-screen">
     <Navbar />
+    <HeroSection />
   </div>
 </template>
 <script setup>
 import { defineAsyncComponent } from 'vue'
 const Navbar = defineAsyncComponent(() => import('@/components/NavBar.vue'))
+const HeroSection = defineAsyncComponent(
+  () => import('@/components/HeroSection.vue'),
+)
 </script>
 
 <style scoped></style>
