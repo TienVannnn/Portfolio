fb4fb41a40022b03b3871ffcd8f2f6c0f2076bca
Tiến <letien3413@gmail.com>
2024-11-04 10:58:21 +0700

    add footer, backToTop, loading

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="bg-[#111827] min-h-screen">
-    <Navbar />
-    <HeroSection />
-    <Skills />
-    <Projects />
-    <About />
-    <Contact />
+    <Suspense>
+      <template #default>
+        <div>
+          <Navbar />
+          <HeroSection />
+          <Skills />
+          <Projects />
+          <About />
+          <Contact />
+          <Footer />
+          <BackToTop />
+        </div>
+      </template>
+      <template #fallback>
+        <div class="flex justify-center items-center min-h-screen">
+          <LoadingSpinner />
+        </div>
+      </template>
+    </Suspense>
   </div>
 </template>
 <script setup>
 import { defineAsyncComponent } from 'vue'
+import LoadingSpinner from './components/LoadingSpinner.vue'
 const Navbar = defineAsyncComponent(() => import('@/components/NavBar.vue'))
 const HeroSection = defineAsyncComponent(
   () => import('@/components/HeroSection.vue'),
@@ -21,6 +35,10 @@ const Contact = defineAsyncComponent(
 )
 const Projects = defineAsyncComponent(
   () => import('@/components/ProjectSection.vue'),
+)
+const Footer = defineAsyncComponent(() => import('@/components/Footer.vue'))
+const BackToTop = defineAsyncComponent(
+  () => import('@/components/BackToTop.vue'),
 )
 </script>
 
